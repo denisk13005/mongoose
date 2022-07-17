0a27dfbcd409cc854e1f37f796bcd8071642a374
denisk13005 <kirmanndenis@gmail.com>
2022-07-17 18:36:42 +0200

    test url

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export const getServerSideProps = async () => {
-  const restaurants = await fetch("http://localhost:3000/api/list").then(res => res.json() )
+  const restaurants = await fetch("/api/list").then(res => res.json() )
   console.log(restaurants);
   return {
     props:{
